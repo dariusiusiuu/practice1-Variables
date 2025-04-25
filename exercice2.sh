f617beef9b9a5d860b709323154df67db3e66c4e
dariusiusiuu <dsanchezr@ies-sabadell.cat>
2025-04-25 20:13:06 +0200

    WIP: cambios antes de rebase

---
+++ exercice2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo ""
+read -p "Please enter the name of the variable: " name
+
+echo ""
+read -p "Please enter tha value for the varibale '$name': " value
+
+echo ""
+echo "The variable '$name' has the value: '$value'"
