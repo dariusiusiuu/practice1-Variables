d9c37be7794fc920d00bab5ebcae184312ffc932
dariusiusiuu <dsanchezr@ies-sabadell.cat>
2025-04-27 21:32:17 +0200

    Added exercice9.sh

--- exercice9.sh
+++ exercice9.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
-# The CRACK variable holds a string value
+# a) What do the ##, #, %% and % operators exactly do?
+# The "##" operator removes the largest possible number of characters from the beginning of the string 
+# that match the given pattern.
+# The "#" operator removes the smallest possible number of characters from the beginning of the string 
+# that match the given pattern.
+# The "%%" operator removes the largest possible number of characters from the end of the string 
+# that match the given pattern.
+# The "%" operator removes the smallest possible number of characters from the end of the string 
+# that match the given pattern.
+
+# b) How can they be useful in Bash scripts?
+# These operators are very useful for manipulating and processing strings in Bash scripts. 
+# They can be used to:
+# - Remove specific prefixes or suffixes from a string.
+# - Extract parts of a string, like file names or extensions.
+# - Process data more flexibly, especially when working with paths, file names, or patterns.
+
+# c) Modifying the value of CRACK and observing how the output changes
+
+#Before change:
+CRACK="oriolorioloriol"
+echo "The result of ##*ori is" ${CRACK##*ori}
+echo "The result of #*ori is" ${CRACK#*ori}
+echo "The result of %%ol* is" ${CRACK%%ol*}
+echo "The result of %ori* is" ${CRACK%ori*}
+
+echo ""
+echo ""
+
+#After change:
 CRACK="dariodariodario"
+echo "The result of ##*ori is" ${CRACK##*ori}
+echo "The result of #*ori is" ${CRACK#*ori}
+echo "The result of %%ol* is" ${CRACK%%ol*}
+echo "The result of %ori* is" ${CRACK%ori*}
 
-# The ## operator removes the largest possible number of characters from the start of the string that match the pattern "dar"
-# What gets removed is everything before the last occurrence of "dar", leaving "io"
-echo "The result of ##*dar is" ${CRACK##*dar}
+# Comments at the end of the script:
 
-# The # operator removes the smallest possible number of characters from the start of the string that match the pattern "dar"
-# In this case, it removes "dar", leaving "iodariodario"
-echo "The result of #*dar is" ${CRACK#*dar}
-
-# The %% operator removes the largest possible number of characters from the end of the string that match the pattern "io"
-# What gets removed is everything after the last occurrence of "io", leaving "dariodari"
-echo "The result of %%io* is" ${CRACK%%io*}
-
-# The % operator removes the smallest possible number of characters from the end of the string that match the pattern "io"
-# What gets removed is everything after the first occurrence of "io", leaving "dariodari"
-echo "The result of %io* is" ${CRACK%io*}
+# Before changing the value of CRACK (CRACK="oriolorioloriol"):
+# ##*ori: The ## operator removes everything before the last occurrence of the pattern "ori". So, from "oriolorioloriol", it removes everything up to the last "ori", leaving "ol".
+# #*ori: The # operator removes the minimum possible match of the pattern "ori". In this case, it only removes the first occurrence of "ori", leaving "olorioloriol".
+# %%ol*: The %% operator removes the maximum possible match from the end of the string that matches the pattern "ol". It removes everything after the last occurrence of "ol", leaving "oriol".
+# %ori*: The % operator removes the minimum possible match from the end of the string that matches the pattern "ori". It removes everything after the first occurrence of "ori", leaving "oloriol".
+# After changing the value of CRACK (CRACK="dariodariodario"):
+# ##*ori: Now, with the new value "dariodariodario", the ## operator removes everything up to the last occurrence of "ori", leaving "o".
+# #*ori: Here, the # operator removes the first occurrence of "ori", leaving "odariodario".
+# %%ol*: With the new value, the %% operator removes everything after the last occurrence of "ol", leaving "dariodari".
+# %ori*: Finally, the % operator removes everything after the first occurrence of "ori", leaving "dariodari".
