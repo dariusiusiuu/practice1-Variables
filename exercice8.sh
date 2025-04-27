cfcfae794ef6f07d4ad6323c83f09a12fcb94086
dariusiusiuu <dsanchezr@ies-sabadell.cat>
2025-04-27 21:01:01 +0200

    Added exercice8.sh

---
+++ exercice8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Enter your username: " username
+
+echo ""
+echo -n "Enter your password: "
+read -s password
+
+echo ""
+echo ""
+echo "Username: $username, Password: $password"
